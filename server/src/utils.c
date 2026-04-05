35fc43f829cce1d33d45583ae1d5bc2e696e0f02
demedm <denisemedinaam@gmail.com>
2026-04-05 00:59:41 +0000

    agrego los cambios en modulo server

--- server/src/utils.c
+++ server/src/utils.c
@@ -8,6 +8,7 @@ int iniciar_servidor(void)
 	assert(!"no implementado!");
 
 	int socket_servidor;
+	int err;
 
 	struct addrinfo hints, *servinfo, *p;
 
@@ -16,13 +17,19 @@ int iniciar_servidor(void)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	err = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
+	int fd_escucha = socket(server_info->ai_family,
+							server_info->ai_socktype,
+							server_info->ai_protocol);
 
 	// Asociamos el socket a un puerto
 
 	// Escuchamos las conexiones entrantes
+	err = setsockopt(fd_escucha, SQL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
+	err = bind(fd_escucha, server_info->ai_addr, server_info->ai_addrlen);
+	err = listen(fd_escucha, SOMAXCONN);
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -33,10 +40,10 @@ int iniciar_servidor(void)
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
